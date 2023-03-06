17f526674ccae57c77288ef97e9c1913124b5e0b
ldk199662 <ldk199662@gmail.com>
2023-03-06 17:38:23 +0900

    styled components setting

--- app/src/components/LIst.tsx
+++ app/src/components/LIst.tsx
@@ -1,4 +1,16 @@
+import styled from 'styled-components';
+
+const Mid = styled.div`
+	background-color: blue;
+`;
+
 const List = () => {
-	return <div>0</div>;
+	return (
+		<>
+			<div>0</div>
+			<Mid>1</Mid>
+			<div>2</div>
+		</>
+	);
 };
 export default List;
