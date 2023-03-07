d46e7ae916238bcac921e5ee3b0660505e297b27
ldk199662 <ldk199662@gmail.com>
2023-03-07 20:29:45 +0900

    add

--- app/src/components/LIst.tsx
+++ app/src/components/LIst.tsx
@@ -1,15 +1,23 @@
 import styled from 'styled-components';
 
+const Top = styled.div`
+	background-color: black;
+`;
+
 const Mid = styled.div`
 	background-color: blue;
+`;
+
+const Foot = styled.div`
+	background-color: pink;
 `;
 
 const List = () => {
 	return (
 		<>
-			<div>0</div>
+			<Top>0</Top>
 			<Mid>1</Mid>
-			<div>2</div>
+			<Foot>2</Foot>
 		</>
 	);
 };
